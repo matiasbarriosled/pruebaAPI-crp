5105bb4bf4115f40dadc5480b2c59eef1750d441
matiasbarriosled <matiasbarriosled@gmail.com>
2024-06-07 09:16:35 -0300

    se cambia a base de datos la obtencion de los precios de btc_usdt

--- database.sql
+++ database.sql
@@ -1,24 +1,4 @@
-CREATE DATABASE CryptoDB;
-GO
-
-USE CryptoDB;
-GO
-
--- Tabla para la información básica de la criptomoneda
-CREATE TABLE Criptomonedas (
-    CriptoID INT PRIMARY KEY IDENTITY,
-    Nombre NVARCHAR(100) NOT NULL,
-    Simbolo NVARCHAR(10) NOT NULL,
-    Descripcion NVARCHAR(255) NULL
-);
-
--- Tabla para almacenar el historial de precios
-CREATE TABLE HistorialPrecios (
-    PrecioID INT PRIMARY KEY IDENTITY,
-    CriptoID INT,
-    Fecha DATETIME NOT NULL,
-    Precio DECIMAL(18, 8) NOT NULL,
-    Volumen DECIMAL(18, 8) NULL,
-    MarketCap DECIMAL(18, 8) NULL,
-    FOREIGN KEY (CriptoID) REFERENCES Criptomonedas(CriptoID)
-);
+CREATE DATABASE backtesting;
+CREATE USER 'tu_usuario'@'localhost' IDENTIFIED BY 'tu_contraseña';
+GRANT ALL PRIVILEGES ON backtesting.* TO 'tu_usuario'@'localhost';
+FLUSH PRIVILEGES;
